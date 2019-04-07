f5f3a446ce4793e7a850b1e63ad11448310efbd5
IsaacVerm <isaacverm@gmail.com>
2019-04-07 21:44:11 +0200

    Add axios to packages and setup basic request example

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+// Vue.component('master-cards', {
+
+// });
+
 var app = new Vue({
   el: '#app',
-  data: {},
+  data: { cards: null },
+  mounted() {
+    var bodyFormData = new FormData();
+    bodyFormData.set('cardId', 1);
+    bodyFormData.set('photoUrl', 'www.photo.com/1');
+    bodyFormData.set('name', 'A pretty postcard');
+    bodyFormData.set('description', 'Look at how pretty it is.');
+    bodyFormData.set('creationDate', '2018-01-05');
+    axios.post('http://127.0.0.1:5000/postcard', bodyFormData).then(() => {
+      axios.get('http://127.0.0.1:5000/postcards').then(response => (this.cards = response));
+    });
+  },
 });
