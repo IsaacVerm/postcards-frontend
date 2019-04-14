4416e683486d83b299a9e0c09bc8fd9ee8994d60
IsaacVerm <isaacverm@gmail.com>
2019-04-14 21:47:21 +0200

    Simplify setCards and setTableHeaders methods

--- README.md
+++ README.md
@@ -13,3 +13,5 @@ Use `methods` instead of `computed` properties in order to fetch data in the rig
 [Making HTTP requests with axios](https://www.sitepoint.com/fetching-data-third-party-api-vue-axios/)
 
 [How to make table data not linked to template](https://www.developerdrive.com/2017/07/creating-a-data-table-in-vue-js/)
+
+[How to chain axios requests](https://github.com/axios/axios/issues/708)
--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 var app = new Vue({
   el: '#app',
-  data: { cards: null, tableHeaders: null },
+  data: { cards: null, tableHeaders: null, apiUrl: 'http://127.0.0.1:5000/postcards' },
   methods: {
-    getPostcards: function() {
-      axios.get('http://127.0.0.1:5000/postcards').then(response => {
-        this.cards = response.data['cards'];
-      });
+    setCards: function(response) {
+      this.cards = response.data['cards'];
     },
-    setTableHeaders: function() {
-      this.tableHeaders = Object.keys(this.cards[0]);
+    setTableHeaders: function(cards) {
+      this.tableHeaders = Object.keys(cards[0]); // all cards have the same headers so just pick one
     },
   },
   created: function() {
-    console.log('lol');
-    axios.get('http://127.0.0.1:5000/postcards').then(response => {
-      this.cards = response.data['cards'];
-      this.tableHeaders = Object.keys(this.cards[0]);
+    axios.get(this.apiUrl).then(response => {
+      this.setCards(response);
+      this.setTableHeaders(this.cards);
     });
   },
 });
