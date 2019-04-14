33a0682ac65b7e3ad023ffefe78e0c2ca558b66d
IsaacVerm <isaacverm@gmail.com>
2019-04-14 22:13:35 +0200

    Add navigation bar

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 var app = new Vue({
   el: '#app',
-  data: { cards: null, tableHeaders: null, apiUrl: 'http://127.0.0.1:5000/postcards' },
+  data: {
+    cards: null,
+    tableHeaders: null,
+    apiUrl: 'http://127.0.0.1:5000/postcards',
+    show: null,
+  },
   methods: {
     setCards: function(response) {
       this.cards = response.data['cards'];
     },
     setTableHeaders: function(cards) {
       this.tableHeaders = Object.keys(cards[0]); // all cards have the same headers so just pick one
+    },
+    setShow: function(component) {
+      this.show = component;
     },
   },
   created: function() {
